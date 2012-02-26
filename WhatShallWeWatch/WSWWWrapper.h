fc6d43ef0cdd1a585e51b47c757ce200deb8b896
neiled <neil@plasticwater.com>
2012-02-26 19:46:07 +0000

    added wrapper

---
+++ WhatShallWeWatch/WSWWWrapper.h
@@ -0,0 +1,16 @@
+//
+//  WSWWWrapper.h
+//  WhatShallWeWatch
+//
+//  Created by Neil Edwards on 26/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WSWWWrapper : NSObject
+
++ (NSArray *) searchForFilmWithTitle:(NSString *)filmName;
+
+
+@end
---
+++ WhatShallWeWatch/WSWWWrapper.m
@@ -0,0 +1,18 @@
+//
+//  WSWWWrapper.m
+//  WhatShallWeWatch
+//
+//  Created by Neil Edwards on 26/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "WSWWWrapper.h"
+
+@implementation WSWWWrapper
+
++ (NSArray *) searchForFilmWithTitle:(NSString *)filmName
+{
+    return nil;
+}
+
+@end
