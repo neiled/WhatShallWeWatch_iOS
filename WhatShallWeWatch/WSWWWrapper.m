0b16a08f4554e4b86d4308d19e488315f67cb93f
Neil Edwards <neil@plasticwater.com>
2012-03-05 22:38:39 +0000

    wrapper uses API

--- WhatShallWeWatch/WSWWWrapper.m
+++ WhatShallWeWatch/WSWWWrapper.m
@@ -8,6 +8,7 @@
 
 #import "WSWWWrapper.h"
 #import "Film.h"
+#import "AFNetworking.h"
 #import <stdlib.h>
 
 @implementation WSWWWrapper
@@ -15,10 +16,20 @@
 + (NSArray *) searchForFilmWithTitle:(NSString *)filmName
 {
     Film* foundFilm = [[Film alloc] init];
-    foundFilm.filmName = @"The Artist";
-    foundFilm.filmYear = @"2011";
-    foundFilm.rating = arc4random() % 100;
-    return [NSArray arrayWithObject:foundFilm];
+//    foundFilm.filmName = @"The Artist";
+//    foundFilm.filmYear = @"2011";
+//    foundFilm.rating = arc4random() % 100;
+//    return [NSArray arrayWithObject:foundFilm];
+
+    NSURL *url = [NSURL URLWithString:@"http://localhost.com:4567/api/v1/film_search/artist"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+        NSLog(@"Results:%@", JSON);
+        //NSLog(@"Name: %@ %@", [JSON valueForKeyPath:@"first_name"], [JSON valueForKeyPath:@"last_name"]);
+    } failure:nil];
+
+    [operation start];
 }
 
 @end
